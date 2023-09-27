b06ba2233a63da9f8123a1c8abb62ea1cec0128d
Unai Egusquiza <145966853+UnaiE@users.noreply.github.com>
2023-09-27 10:46:11 +0200

    Hello mmgv

--- Gitejemplo/src/Holamundo.java
+++ Gitejemplo/src/Holamundo.java
@@ -1,7 +1,7 @@
 
 public class Holamundo {
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hello word");
 		System.out.println("segundo cambio de prueba!");
 		
 	}
